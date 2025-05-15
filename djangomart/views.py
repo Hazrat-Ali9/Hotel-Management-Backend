4de5934739f61313ba8cf55c4b881a06f2cfb628
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-15 16:54:00 +0600

    views   🚂

--- djangomart/views.py
+++ djangomart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from store.models import Product
-
+# views
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
