6a09ddbcf048865de7c65296c8d2118f6fbeeae0
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-05-26 22:26:34 +0600

    models :tada:

--- store/models.py
+++ store/models.py
@@ -3,7 +3,7 @@ from category.models import Category
 from django.urls import reverse
 from accounts.models import Account
 from django.db.models import Avg, Count
-
+# models
 class Product(models.Model):
     product_name    = models.CharField(max_length=200, unique=True)
     slug            = models.SlugField(max_length=200, unique=True)
